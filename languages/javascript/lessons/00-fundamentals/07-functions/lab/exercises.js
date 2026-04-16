769d8e6248a06d110eb83ec279816ddcf190296e
netanel segal <segal.netanel4@gmail.com>
2026-04-16 15:49:55 +0300

    up

--- languages/javascript/lessons/00-fundamentals/07-functions/lab/exercises.js
+++ languages/javascript/lessons/00-fundamentals/07-functions/lab/exercises.js
@@ -9,23 +9,20 @@
 
 // Task 2: Function with Return
 // 1. Write a function 'multiply' that takes two numbers.
-// 2. It should RETURN the product of the two numbers.
+// 2. It should RETURN the product of the two numbers using addition only.
 // 3. Call the function and store the result in a variable 'res'.
 // 4. Print 'res'.
 // Write your code below:
 
 
-// Task 3: Arrow Functions
-// 1. Convert this function declaration into an arrow function:
-/*
-function getCircleArea(radius) {
-  return 3.14 * radius * radius;
-}
-*/
-// Write your arrow function below:
+// Task 3: Circle Area
+// 1. write a function that gets a raduis and returns its area:.
+// 2. call the funtion save the return value in a veriable calles area
+// 3. print the result in a nice message
+// Write your function below:
 
 
-// Task 4: Power function
+// Task 4: Power function with multiplication only
 // 1. Write a function 'pow(x, n)' that returns x in power n.
 // 2. (Assume n is a positive integer).
 // Example: pow(3, 2) = 3 * 3 = 9.
